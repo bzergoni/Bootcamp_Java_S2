99ba25f853a6816db2d5e4a15154b09532835685
Bruno Zergoni <bruno_z_c@hotmail.com>
2018-06-28 11:21:30 -0300

    No funcional - no lo levanta el servidor

--- src/main/java/com/globant/bootcamp/service/forecastService.java
+++ src/main/java/com/globant/bootcamp/service/forecastService.java
@@ -5,15 +5,14 @@ import java.util.*;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+//import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.ext.Provider;
+//import javax.ws.rs.ext.Provider;
 
 import com.globant.bootcamp.domain.*;
 import org.springframework.stereotype.Component;
 
 
-//@Provider
 @Component
 @Path("/forecast")
 @Produces("application/json")
